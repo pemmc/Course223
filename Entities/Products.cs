f40ad8da2d60cc858f5d6264c5617c854141f512
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-27 13:34:13 -0300

    Initial check-in of module Course223

---
+++ Entities/Products.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Globalization;
+
+namespace Predicate.Entities
+{
+    public class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+ 
+        public Products(string name, double price)
+        {
+            Name = name;
+            Price = price;
+
+        }
+
+        public override string ToString()
+        {
+            return "Nome: "
+                    + Name
+                    + ", "
+                    + Price.ToString("C", new CultureInfo("pt-BR"));
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using Predicate.Entities;
+
+namespace Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Products> list = new List<Products>();
+            list.Add(new Products("Tv", 900.00));
+            list.Add(new Products("Mouse", 50.00));
+            list.Add(new Products("Tablet", 350.50));
+            list.Add(new Products("HD Case", 80.90));
+
+            //PREDICATO...
+            //É uma função que recebe um objeto e retorna um bool
+            list.RemoveAll(p => p.Price >= 100.0);
+
+            //funciona tb
+            //list.RemoveAll(p => p.Price >= 100.0);
+
+            Console.WriteLine("--- LISTA DE PRODUTOS COM VALORES ACIMA DE 100.0---");
+            foreach (Products p in list)
+            {
+                Console.WriteLine(p);
+
+            }
+        }
+
+    }
+}
